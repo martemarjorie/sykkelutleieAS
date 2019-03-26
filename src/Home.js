de118d84b81e29d09acf5b0bed69074f65ac9de8
Carlerndm <cg.dm@hotmail.com>
2019-03-26 10:27:25 +0100

    Revert "Merge branch 'master' of https://github.com/martemarjorie/sykkelutleieAS"
    This reverts commit 927ac9004ce7d3365ec55caec7b660318652e4e1, reversing
    changes made to 75fdde0d01036a44ee1ce8f68a1cfe9151c1cac3.

--- src/Home.js
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import { Component } from 'react-simplified';
-
-export default class Home extends Component {
-  render() {
-    return <div>SykkelUtleie AS test</div>;
-  }
-}
