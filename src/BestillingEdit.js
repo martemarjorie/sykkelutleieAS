0cde955f6d89504e239e469930e078ff5b9e88e6
Marte Marjorie Søgnen <marte.sognen@hotmail.com>
2019-03-27 14:57:23 +0100

    oversikt funker ikke

---
+++ src/BestillingEdit.js
@@ -0,0 +1,137 @@
+import * as React from 'react';
+import { Component } from 'react-simplified';
+import ReactDOM from 'react-dom';
+import { NavLink, HashRouter, Route } from 'react-router-dom';
+import { bestillingService } from './services';
+import { Card, Row, Column, NavBar, Button, Form } from './widgets';
+
+export default class BestillingEdit extends Component {
+  type_sykkel = '';
+  modell = '';
+  utlev_tidspunkt = '';
+  innlev_tidspunkt = '';
+  utlev_sted = '';
+  innlev_sted = '';
+  fornavn = '';
+  tlf = '';
+
+  render() {
+    return (
+      <form>
+        <table>
+          <tbody>
+            <tr>
+              <tr>
+                Type:{' '}
+                <input
+                  type="text"
+                  value={this.type_sykkel}
+                  onChange={event => (this.type_sykkel = event.target.value)}
+                />
+              </tr>
+              <tr>
+                Modell: <input type="text" value={this.modell} onChange={event => (this.modell = event.target.value)} />
+              </tr>
+              <tr>
+                Utleveringstidspunkt:{' '}
+                <input
+                  type="number"
+                  value={this.utlev_tidspunkt}
+                  onChange={event => (this.utlev_tidspunkt = event.target.value)}
+                />
+              </tr>
+              <tr>
+                Innleveringstidspunkt:{' '}
+                <input
+                  type="text"
+                  value={this.innlev_tidspunkt}
+                  onChange={event => (this.innlev_tidspunkt = event.target.value)}
+                />
+              </tr>
+              <tr>
+                Utleveringssted:{' '}
+                <input type="text" value={this.utlev_sted} onChange={event => (this.utlev_sted = event.target.value)} />
+              </tr>
+              <tr>
+                <tr>
+                  Innleveringssted:{' '}
+                  <input
+                    type="text"
+                    value={this.innlev_sted}
+                    onChange={event => (this.innlev_sted = event.target.value)}
+                  />
+                </tr>
+                <tr>
+                  Fornavn:{' '}
+                  <input type="text" value={this.fornavn} onChange={event => (this.fornavn = event.target.value)} />
+                </tr>
+                <tr>
+                  Tlf: <input type="text" value={this.tlf} onChange={event => (this.tlf = event.target.value)} />
+                </tr>
+                <Row>
+                  <Column left>
+                    <button type="button" onClick={this.save}>
+                      Lagre
+                    </button>
+                  </Column>
+                  <Column right>
+                    <button type="button" onClick={this.delete}>
+                      Slett
+                    </button>
+                  </Column>
+                </Row>
+              </tr>
+            </tr>
+          </tbody>
+        </table>
+      </form>
+    );
+  }
+
+  mounted() {
+    bestillingService.getBestillingsinfoer(this.props.match.params.person_bestilling.person_id, bestillingsinfo => {
+      this.type_sykkel = bestillingsinfo.type_sykkel;
+      this.modell = bestillingsinfo.modell;
+      this.utlev_tidspunkt = bestillingsinfo.utlev_tidspunkt;
+      this.innlev_tidspunkt = bestillingsinfo.innlev_tidspunkt;
+      this.utlev_sted = bestillingsinfo.utlev_sted;
+      this.innlev_sted = bestillingsinfo.innlev_sted;
+      this.fornavn = bestillingsinfo.fornavn;
+      this.tlf = bestillingsinfo.tlf;
+    });
+  }
+
+  save() {
+    bestillingService.updateBestillingsinfoer(
+      this.props.match.params.person_bestilling.person_id,
+      this.type_sykkel,
+      this.modell,
+      this.utlev_tidspunkt,
+      this.innlev_tidspunkt,
+      this.utlev_sted,
+      this.innlev_sted,
+      this.fornavn,
+      this.tlf,
+      () => {
+        history.push('/bestillingsinfoer');
+      }
+    );
+  }
+
+  delete() {
+    bestillingService.deleteBestillingsinfoer(
+      this.props.match.params.person_bestilling.person_id,
+      this.type_sykkel,
+      this.modell,
+      this.utlev_tidspunkt,
+      this.innlev_tidspunkt,
+      this.utlev_sted,
+      this.innlev_sted,
+      this.fornavn,
+      this.tlf,
+      () => {
+        history.push('/bestillingsinfoer');
+      }
+    );
+  }
+}
---
+++ src/BestillingsInfo.js
@@ -0,0 +1,64 @@
+import * as React from 'react';
+import { Component } from 'react-simplified';
+import ReactDOM from 'react-dom';
+import { NavLink, HashRouter, Route } from 'react-router-dom';
+import { bestillingService } from './services';
+import { Card, Row, Column, NavBar, Button, Form } from './widgets';
+
+export default class BestillingsInfo extends Component {
+  bestillingsinfoer = [];
+
+  render() {
+    return (
+      <Card title="Kundeliste">
+        Oversikt over alle registrerte kunder:
+        {this.bestillingsinfoer.map(bestillingsinfo => (
+          <Row key={bestillingsinfo.person_id}>
+            <Column>
+              <NavLink to={'/bestillingsinfoer/' + bestillingsinfo.type_sykkel + '/edit'}>
+                {bestillingsinfo.type_sykkel}
+              </NavLink>
+            </Column>
+            <Column>
+              <NavLink to={'/bestillingsinfoer/' + bestillingsinfo.modell + '/edit'}>{bestillingsinfo.modell}</NavLink>
+            </Column>
+            <Column>
+              <NavLink to={'/bestillingsinfoer/' + bestillingsinfo.utlev_tidspunkt + '/edit'}>
+                {bestillingsinfo.utlev_tidspunkt}
+              </NavLink>
+            </Column>
+            <Column>
+              <NavLink to={'/bestillingsinfoer/' + bestillingsinfo.innlev_tidspunkt + '/edit'}>
+                {bestillingsinfo.innlev_tidspunkt}
+              </NavLink>
+            </Column>
+            <Column>
+              <NavLink to={'/bestillingsinfoer/' + bestillingsinfo.utlev_tidspunkt + '/edit'}>
+                {bestillingsinfo.utlev_tidspunkt}
+              </NavLink>
+            </Column>
+            <Column>
+              <NavLink to={'/bestillingsinfoer/' + bestillingsinfo.innlev_sted + '/edit'}>
+                {bestillingsinfo.innlev_sted}
+              </NavLink>
+              <Column>
+                <NavLink to={'/bestillingsinfoer/' + bestillingsinfo.fornavn + '/edit'}>
+                  {bestillingsinfo.fornavn}
+                </NavLink>
+              </Column>
+              <Column>
+                <NavLink to={'/bestillingsinfoer/' + bestillingsinfo.tlf + '/edit'}>{bestillingsinfo.tlf}</NavLink>
+              </Column>
+            </Column>
+          </Row>
+        ))}
+      </Card>
+    );
+  }
+
+  mounted() {
+    bestillingService.getBestillingsinfoer(bestillingsinfoer => {
+      this.bestillingsinfoer = bestillingsinfoer;
+    });
+  }
+}
