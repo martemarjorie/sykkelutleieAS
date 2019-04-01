d6a02f08643476a157c43904c519a287db1a0560
Emilie <emilie.fleischer@gmail.com>
2019-04-01 13:51:54 +0200

    Endret stil på meny

--- src/Menu.js
+++ src/Menu.js
@@ -3,53 +3,48 @@ import { Component } from 'react-simplified';
 import ReactDOM from 'react-dom';
 import { NavLink, HashRouter, Route } from 'react-router-dom';
 import { personService } from './services';
-import { Card, Column, NavBar, Button, Form } from './widgets';
 import { Container, Col, Row } from 'react-bootstrap';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 
 export default class Menu extends Component {
   render() {
     return (
-      <div style={{ height: '40px', backgroundColor: 'lightblue', paddingTop: '11px' }}>
-        <Container>
-          <Row>
-            <Col>
-              <NavLink style={{ color: 'black' }} exact activeStyle={{ color: 'peru' }} to="/">
-                Hjem
-              </NavLink>
-            </Col>
-            <Col>
-              <NavLink style={{ color: 'black' }} exact activeStyle={{ color: 'peru' }} to="/bestillinger">
-                Ny bestilling
-              </NavLink>
-            </Col>
-            <Col>
-              <NavLink style={{ color: 'black' }} exact activeStyle={{ color: 'peru' }} to="/persons">
-                Kunde
-              </NavLink>
-            </Col>
-            <Col>
-              <NavLink style={{ color: 'black' }} exact activeStyle={{ color: 'peru' }} to="/sykler">
-                Sykkel
-              </NavLink>
-            </Col>
-            <Col>
-              <NavLink style={{ color: 'black' }} exact activeStyle={{ color: 'peru' }} to="/bestillingsinfoer">
-                Bestillingsoversikt
-              </NavLink>
-            </Col>
-            <Col>
-              <NavLink style={{ color: 'black' }} exact activeStyle={{ color: 'peru' }} to="/utstyrer">
-                Utstyr
-              </NavLink>
-            </Col>
-            <Col>
-              <NavLink style={{ color: 'black' }} exact activeStyle={{ color: 'peru' }} to="/transport">
-                Transport
-              </NavLink>
-            </Col>
-          </Row>
-        </Container>
-      </div>
+      <Navbar bg="light" variant="light">
+        <Navbar.Brand href="#">Sykkelutleie</Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link>
+            <NavLink style={{ color: 'grey' }} exact activeStyle={{ color: 'black' }} to="/">
+              Hjem
+            </NavLink>
+          </Nav.Link>
+          <Nav.Link>
+            <NavLink style={{ color: 'grey' }} exact activeStyle={{ color: 'black' }} to="/bestillinger">
+              Ny bestilling
+            </NavLink>
+          </Nav.Link>
+          <Nav.Link>
+            <NavLink style={{ color: 'grey' }} exact activeStyle={{ color: 'black' }} to="/persons">
+              Kunder
+            </NavLink>
+          </Nav.Link>
+          <Nav.Link>
+            <NavLink style={{ color: 'grey' }} exact activeStyle={{ color: 'black' }} to="/sykler">
+              Sykler
+            </NavLink>
+          </Nav.Link>
+          <Nav.Link>
+            <NavLink style={{ color: 'grey' }} exact activeStyle={{ color: 'black' }} to="/utstyrer">
+              Utstyr
+            </NavLink>
+          </Nav.Link>
+          <Nav.Link>
+            <NavLink style={{ color: 'grey' }} exact activeStyle={{ color: 'black' }} to="/transport">
+              Transport
+            </NavLink>
+          </Nav.Link>
+        </Nav>
+      </Navbar>
     );
   }
 }
